3cddaa8078fe24b84bffd097270bfd6ef629b564
WChienMing <86589658+WChienMing@users.noreply.github.com>
2023-08-31 19:51:32 +0800

    update

--- verify.php
+++ verify.php
@@ -1,15 +1,32 @@
 <?php
 header("Content-Type: application/json");
 
+$db_host = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "privileged";
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+
 $json = file_get_contents('php://input');
 $obj = json_decode($json, true);
+$codeFromClient = $obj["password"];
 
-$correctPassword = "12345";
-$password = $obj["password"];
+$stmt = $conn->prepare("SELECT * FROM private_code WHERE code = ?");
+$stmt->bind_param("s", $codeFromClient);
 
-if ($password === $correctPassword) {
-  echo json_encode(["status" => "success", "code" => $correctPassword]);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    echo json_encode(["status" => "success", "message" => "验证成功"]);
 } else {
-  echo json_encode(["status" => "error"]);
+    echo json_encode(["status" => "error", "message" => "代码错误"]);
 }
+
+$stmt->close();
+$conn->close();
 ?>
